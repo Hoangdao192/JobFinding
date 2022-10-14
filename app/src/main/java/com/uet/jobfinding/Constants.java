b5f2aa66a5bc080fea45526f71e3179165f579e6
Hoangdao192 <nguyendanghoangdao2002@gmail.com>
2022-10-14 20:49:46 +0700

    Add dependency

---
+++ app/src/main/java/com/uet/jobfinding/Constants.java
@@ -0,0 +1,5 @@
+package com.uet.jobfinding;
+
+public class Constants {
+    public static final String DATABASE_URL = "https://ecommerceapplication-10d5c-default-rtdb.asia-southeast1.firebasedatabase.app/";
+}
