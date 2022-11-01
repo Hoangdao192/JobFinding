83692160be39546eafe8096e88d9d282127c86bc
Hoangdao192 <nguyendanghoangdao2002@gmail.com>
2022-11-01 18:58:47 +0700

    Test login with facebook

--- app/src/main/java/com/uet/fwork/MainActivity.java
+++ app/src/main/java/com/uet/fwork/MainActivity.java
@@ -4,9 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Pair;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseUser;
 import com.uet.fwork.account.login.LoginActivity;
 import com.uet.fwork.util.VietNameAdministrativeDivisionAPI;
 
@@ -17,18 +22,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (firebaseUser != null) {
+            firebaseUser.reload().addOnSuccessListener(
+                    new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            startActivity(new Intent(MainActivity.this, HelloActivity.class));
+                        }
+                    }
+            )
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            e.printStackTrace();
+                            if (e instanceof FirebaseAuthInvalidUserException) {
+                                System.out.println("USER HAS BEEN DELETED");
+                                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                            }
+                        }
+                    });
+        } else {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
-        } else {
-            System.out.println(FirebaseAuth.getInstance().getUid());
-            System.out.println(FirebaseAuth.getInstance().getCurrentUser().getUid());
-            startActivity(new Intent(this, HelloActivity.class));
         }
-
-//        VietNameAdministrativeDivisionAPI api = VietNameAdministrativeDivisionAPI.getInstance();
-//        api.getProvinceList(this, result -> {
-//
-//        });
     }
 }
--- app/src/main/java/com/uet/fwork/account/login/LoginActivity.java
+++ app/src/main/java/com/uet/fwork/account/login/LoginActivity.java
@@ -1,13 +1,45 @@
 package com.uet.fwork.account.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.uet.fwork.HelloActivity;
 import com.uet.fwork.R;
+import com.uet.fwork.database.model.UserModel;
+import com.uet.fwork.database.model.UserRole;
+import com.uet.fwork.database.repository.UserRepository;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
+    private RelativeLayout btnLoginWithFacebook;
+    private LoginManager loginManager;
+    private CallbackManager callbackManager;
+    private FirebaseAuth firebaseAuth;
+    private UserRepository userRepository;
+
     public LoginActivity() {
         super();
     }
@@ -16,5 +48,91 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userRepository = new UserRepository(FirebaseDatabase.getInstance());
+        firebaseAuth = FirebaseAuth.getInstance();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        loginManager = LoginManager.getInstance();
+        this.callbackManager = CallbackManager.Factory.create();
+        btnLoginWithFacebook = (RelativeLayout) findViewById(R.id.btnLoginFacebook);
+        loginManager.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("FACEBOOK", "Callback success");
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("FACEBOOK", "facebook:onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                error.printStackTrace();
+                Log.d("FACEBOOK", "facebook:error");
+            }
+        });
+
+        btnLoginWithFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginManager.logInWithReadPermissions(LoginActivity.this, Arrays.asList("email", "public_profile"));
+            }
+        });
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+//        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+
+        firebaseAuth.signInWithCredential(credential).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Log.d("FACEBOOK", "onSuccess");
+                userRepository.isUserExists(
+                        firebaseAuth.getUid(),
+                        result -> {
+                            if (!result) {
+                                initUserData();
+                                System.out.println(firebaseAuth.getCurrentUser().getEmail());
+                                Intent intent = new Intent(LoginActivity.this, HelloActivity.class);
+                                startActivity(intent);
+                            } else {
+                                Intent intent = new Intent(LoginActivity.this, HelloActivity.class);
+                                startActivity(intent);
+                            }
+                        });
+
+//                System.out.println(firebaseAuth.getCurrentUser().getEmail());
+//                Intent intent = new Intent(getActivity(), HelloActivity.class);
+//                getActivity().startActivity(intent);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("FACEBOOK", "activity result");
+        // Pass the activity result back to the Facebook SDK
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void initUserData() {
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        UserModel userModel = new UserModel(
+                firebaseUser.getUid(), firebaseUser.getEmail(),
+                "", "", "", "", ""
+        );
+        userRepository.insertUser(userModel);
     }
 }
